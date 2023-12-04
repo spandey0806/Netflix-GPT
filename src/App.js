93c3acf748b8ff626687c8022f443331691e7054
Shubham Pandey <shubhampandey0806@gmail.com>
2023-12-04 11:28:03 +0530

    CreateReactApp and TailwindCSS configured

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-600" >
+     Namaste Everyone  h
+    </div>
+  );
+}
+
+
+export default App;
